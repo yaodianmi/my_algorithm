6a40e742f25eed4713bd17822a70b149f9f1cae2
yaodianmi <miyao.programmer@gmail.com>
2017-08-23 17:15:12 +0800

    add example

--- caesars_cipher.js
+++ caesars_cipher.js
@@ -5,6 +5,12 @@
  *移位密码也就是密码中的字母会按照指定的数量来做移位。
  *一个常见的案例就是ROT13密码，字母会移位13个位置。
  *由'A' ↔ 'N', 'B' ↔ 'O'，以此类推。
+ *所有的字母都是大写，不要转化任何非字母形式的字符(例如：空格，标点符号),
+ *遇到这些特殊字符，跳过它们。
+ *rot13("SERR PBQR PNZC") 应该解码为 "FREE CODE CAMP"
+ *rot13("SERR CVMMN!") 应该解码为 "FREE PIZZA!"
+ *rot13("SERR YBIR?") 应该解码为 "FREE LOVE?"
+ *rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.") 应该解码为 "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX."
 */
 
 function rot13(str) {
@@ -26,7 +32,8 @@ function rot13(str) {
     }
     newStr.push(s);
   }
-  
 
   return newStr.join('');
 }
+
+
